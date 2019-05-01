ebd6e321abbc939107d731a477e9d250dd06a45e
Jon Gauthier <jon@gauthiers.net>
2019-05-01 10:16:34 -0400

    gcal API: hard-code the relevant calendars for now

--- gcal.py
+++ gcal.py
@@ -16,6 +16,8 @@ oauth_decorator = OAuth2DecoratorFromClientSecrets(
 
 service = build("calendar", "v3")
 
+INCLUDE = ["jon@gauthiers.net", "MIT classes", "MIT regulars"]
+
 
 def fetch_all_calendar_events(timeMin=None, timeMax=None,
                               calendars=None, preferred_timezones=None):
@@ -23,45 +25,50 @@ def fetch_all_calendar_events(timeMin=None, timeMax=None,
 
     # fetch all calendars
     calendars = service.calendarList().list(maxResults=250).execute(http=http)
-    # DEV: MIT classes calendar ID
-    calendarID = "gauthiers.net_0n5kpl5nf2srmrkstbfefab1m8@group.calendar.google.com"
+    calendarIDs = [c["id"] for c in calendars["items"] if c["summary"] in INCLUDE]
 
-    # TODO do for all calendars
     # https://developers.google.com/resources/api-libraries/documentation/calendar/v3/python/latest/calendar_v3.events.html#list
     all_events = []
     recurring_events = []
-    events_req = service.events().list(calendarId=calendarID, maxResults=2500, # calendarId="primary",
-                                       timeMin=timeMin, timeMax=timeMax)
-    while events_req is not None:
-        events_resp = events_req.execute(http=http)
-        items = events_resp["items"]
+    for calendarID in calendarIDs:
+        events_req = service.events().list(calendarId=calendarID, maxResults=2500,
+                                           timeMin=timeMin, timeMax=timeMax)
+        while events_req is not None:
+            events_resp = events_req.execute(http=http)
+            items = events_resp["items"]
 
-        while items:
-            item = items.pop()
+            while items:
+                item = items.pop()
 
-            if "recurrence" in item:
-                # This is a recurring event. Add to the queue relevant instances today.
-                instances = service.events().instances(
-                        calendarId=calendarID, eventId=item["id"],
-                        maxResults=2500, timeMin=timeMin, timeMax=timeMax) \
-                    .execute(http=http)
-                items.extend(instances["items"])
-            else:
-                start = item.get("start", {})
-                end = item.get("end", {})
+                if "recurrence" in item:
+                    # This is a recurring event. Add to the queue relevant instances today.
+                    instances = service.events().instances(
+                            calendarId=calendarID, eventId=item["id"],
+                            maxResults=2500, timeMin=timeMin, timeMax=timeMax) \
+                        .execute(http=http)
+                    items.extend(instances["items"])
+                else:
+                    if item.get("status") == "cancelled":
+                        continue
+                    elif "dateTime" not in item["start"]:
+                        # all-day event
+                        continue
 
-                all_events.append({
-                    "id": item["id"],
-                    "dt_created": parse(item["created"]) if "created" in item else None,
-                    "dt_updated": parse(item["updated"]) if "updated" in item else None,
-                    "dt_start": parse(start["dateTime"] if "dateTime" in start else start.get("date")),
-                    "dt_end": parse(end["dateTime"] if "dateTime" in end else end.get("date")),
-                    "summary": item.get("summary"),
-                    "description": item.get("description"),
-                    "is_allday": "dateTime" not in start,
-                })
+                    start = item.get("start", {})
+                    end = item.get("end", {})
 
-        events_req = service.events().list_next(events_req, events_resp)
+                    all_events.append({
+                        "id": item["id"],
+                        "dt_created": parse(item["created"]) if "created" in item else None,
+                        "dt_updated": parse(item["updated"]) if "updated" in item else None,
+                        "dt_start": parse(start["dateTime"] if "dateTime" in start else start.get("date")),
+                        "dt_end": parse(end["dateTime"] if "dateTime" in end else end.get("date")),
+                        "summary": item.get("summary"),
+                        "description": item.get("description"),
+                        "is_allday": "dateTime" not in start,
+                    })
+
+            events_req = service.events().list_next(events_req, events_resp)
 
     return all_events
 
